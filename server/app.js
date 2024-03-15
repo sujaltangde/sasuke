05dc0f974a155c3defd8ec9f7768d9b0f2dc6c6f
harshalhonde21 <harshalhonde17@gmail.com>
2024-03-15 12:06:38 +0530

    individual updated success

--- server/app.js
+++ server/app.js
@@ -1,27 +1,28 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const dotenv = require('dotenv')
-dotenv.config({path:'./config/config.env'})
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config/config.env" });
 
-app.use(express.json({ limit: '10mb' }))
+app.use(express.json({ limit: "10mb" }));
 
-app.use(cors({
+app.use(
+  cors({
     origin: "*",
-    credentials: true
-}))
+    credentials: true,
+  })
+);
 
+const user = require("./routes/userRoutes");
+const charity = require("./routes/charityRoutes");
+const individual = require("./routes/individualRoutes");
 
+app.use("/business/", user);
+app.use("/charity/", charity);
+app.use("/individual/", individual);
 
-const user = require('./routes/userRoutes')
-const charity = require('./routes/charityRoutes')
+app.get("/", (req, res) => {
+  res.json("I am working");
+});
 
-app.use("/business/",user)
-app.use("/charity/",charity)
-
-app.get("/",(req,res)=>{
-    res.json("I am working")
-})                           
-    
-
-module.exports = app ;
+module.exports = app;
